761574aca57f43c1d287f6381e86f743888ec21d
andreplayer1 <andreplayer1@gmail.com>
2023-03-11 20:55:40 -0300

    Fim da Aula 1 de JavaScript - Infinity School

---
+++ javascript/Aula 1 - Introdução ao JS/ex02.js
@@ -0,0 +1,16 @@
+let number = 3;
+
+function adicionar() {
+    number ++
+    console.log(number)
+}
+
+function subtrair() {
+    number --
+    console.log(number)
+}
+
+function apagar() {
+    number = 0
+    console.log(number)
+}
