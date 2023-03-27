a98c618297c667bbfd7ecfb57419ac1f9a58e5b5
andreplayer1 <andreplayer1@gmail.com>
2023-03-27 01:40:28 -0300

    Ex01
    Exercício 01 da Aula de Repetições

--- javascript/Aula 3 - Comandos de Repetição/exercicios/ex01/ex01.js
+++ javascript/Aula 3 - Comandos de Repetição/exercicios/ex01/ex01.js
@@ -1,9 +1,11 @@
 function myFunction() {
     let formulario = document.forms["formulario"];
     let entrada = formulario.elements.entrada.value;
-
-    for (let i = entrada; i >= 0; i--) {
-        document.getElementById("resposta").innerHTML += `${i}<br>`
-    }
     
+    let mensagem
+    for(let i = entrada; i != -1 ;i--){
+        mensagem += `Numero ${i} <br>`;
+
+    document.getElementById('resposta').innerHTML = mensagem
+    }
 }
