f11ecf0fd8b568ee2e0023d044797d00e693fd2c
andreplayer1 <andreplayer1@gmail.com>
2023-04-18 06:46:29 -0300

    Revisão da Aula de Funções
    Praticando functions

--- javascript/Aula 5 - Funções/exercicios/ex01.2/ex01.js
+++ javascript/Aula 5 - Funções/exercicios/ex01.2/ex01.js
@@ -1,7 +1,8 @@
 function numQuad() {
     let num = document.getElementById('entrada').value;
+
     function quadrado(numero) {
-    return numero * numero
+        return numero * numero
     }
     document.getElementById('res').innerHTML = quadrado(num)
 }
--- javascript/Aula 5 - Funções/revisao-pos-aula/functions/func.js
+++ javascript/Aula 5 - Funções/revisao-pos-aula/functions/func.js
@@ -1,8 +1,8 @@
-function nome() {
+function entrada() {
     let nom = document.querySelector('input#entrada').value;
 
-    function saudacao(name) {
-        return `Olá, ${name}!`
-    } 
+    function saudacao(nome) {
+        return `Olá, ${nome} Seja bem vindo(a)!`
+    }
     document.getElementById('res').innerHTML = saudacao(nom)
 }
