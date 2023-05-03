1c4964e2c24347825e5c29e22d898e28b02a7fb5
andreplayer1 <andreplayer1@gmail.com>
2023-05-03 00:32:08 -0300

    projeto tela de login
    inicio do projeto

---
+++ javascript/Aula 7 - Revisão-Projeto/Projeto Tela de Login/login/script.js
@@ -0,0 +1,16 @@
+let user = 'andre';
+let password = '1234';
+
+function login(){
+
+    let loginUser = document.getElementById('user').value;
+    let loginPassword = document.getElementById('password').value;
+
+    if (loginUser == user && loginPassword == password) {
+        location.href = "../projeto-pizzaria/index.html"
+    } else if(loginUser.lenght == 0 || loginPassword.length == 0) {
+        msg.innerHTML = ('Digite seu login e senha ou cadastre-se!')
+    } else {
+        msg.innerHTML = ('Login e senha inválidos!')
+    }
+}
