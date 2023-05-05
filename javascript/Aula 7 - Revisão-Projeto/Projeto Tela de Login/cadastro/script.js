7428ff5efd792cdde680495f65fdaec56d8c09c6
andreplayer1 <andreplayer1@gmail.com>
2023-05-04 22:53:11 -0300

    att

--- javascript/Aula 7 - Revisão-Projeto/Projeto Tela de Login/cadastro/script.js
+++ javascript/Aula 7 - Revisão-Projeto/Projeto Tela de Login/cadastro/script.js
@@ -1,12 +1,4 @@
-let confirm = document.getElementById('confirmCad');
-function cadastro() {
-    let pass = document.getElementById('password').value;
-    let confirmPassword = document.getElementById('password2').value;
-    if (pass == confirmPassword) {
-        var password = pass
-        var user = document.getElementById('username').value;
-        confirm.innerHTML = ('Cadastro efetuado!')
-    } else {
-        confirm.innerHTML = ('Senha não confere!')
-    }
+function cadastrar() {
+
+    location.href = "../login/login.html"
 }
