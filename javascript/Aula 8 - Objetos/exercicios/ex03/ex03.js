a887847b511cc6b2ebeae39ea235bd5aff8e4b45
andreplayer1 <andreplayer1@gmail.com>
2023-05-15 06:08:32 -0300

    up

---
+++ javascript/Aula 8 - Objetos/exercicios/ex03/ex03.js
@@ -0,0 +1,61 @@
+//Crie uma estrutura que permita ao usuário adicionar atletas na lista do exercício anterior.
+
+let html = '';
+let container = document.getElementById('container');
+let atletas = [
+    {
+        nome: 'Michael',
+        esporte: 'Volei'
+    },
+
+    {
+        nome: 'Jhon',
+        esporte: 'Basketball'
+    },
+
+    {
+        nome: 'Ítalo',
+        esporte: 'Handball'
+    },
+
+    {
+        nome: 'Joana',
+        esporte: 'Football'
+    },
+
+    {
+        nome: 'Laís',
+        esporte: 'Natação'
+    }
+];
+
+let mostrarNaPagina;
+let listaEsportes;
+let maisPraticado;
+
+let exibir = ()=> {
+    for(let i = 0; i < atletas.length; i++) {
+        mostrarNaPagina += `Nome: ${atletas[i].nome}<br>`
+        mostrarNaPagina += `Esporte: ${atletas[i].esporte}<br>`
+        listaEsportes += `Esporte: ${atletas[i].esporte}<br>`
+    };
+    container.innerHTML = mostrarNaPagina
+}
+
+let adicionar = ()=> {
+    mostrarNaPagina = html
+    let nome = document.getElementById('nome').value;
+    let esporte = document.getElementById('esporte').value;
+    
+    atleta = {
+        nome: nome,
+        esporte: esporte
+    };
+    atletas.push(atleta)
+
+    exibir()
+}
+
+const esporteMaisPraticado = ()=> {
+    
+}
