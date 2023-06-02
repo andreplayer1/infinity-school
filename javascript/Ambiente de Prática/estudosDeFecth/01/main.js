1ff6cfb03eefdc6fcd4c8199d773d6301a5a9da2
andreplayer1 <andreplayer1@gmail.com>
2023-06-01 22:06:16 -0300

    Estudos com fatch

---
+++ javascript/Ambiente de Prática/estudosDeFecth/01/main.js
@@ -0,0 +1,25 @@
+const btnPesquisarCep = document.querySelector("#btnPesquisar");
+btnPesquisarCep.addEventListener("click", event =>{
+    event.preventDefault();
+})
+const inputDoCep = document.querySelector("#cep");
+const valorDoCep = inputDoCep.value;
+const url = `https://viacep.com.br/ws/${valorDoCep}/json/`;
+
+fetch(url).then(response =>{
+    return response.json();
+}).then(data =>{
+    function atribuirCampos(data){
+        const rua = document.querySelector("#rua");
+        const complemento = document.querySelector("#complemento");
+        const bairro = document.querySelector("bairro");
+        const cidade = document.querySelector("cidade");
+        const estado = document.querySelector("estado");
+
+        rua.value = data.logradouro;
+        complemento.value = data.complemento;
+        bairro.value = data.bairro;
+        cidade.value = data.localidade;
+        estado.value = data.uf
+    }
+    })
